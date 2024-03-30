4479c2ee7e80068c4ad104f357f3d5a8073fd438
rahulsbeelur <srahulbeelur@gmail.com>
2024-03-30 22:59:22 +0530

    Created Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 const Home = () => {
-    return <div>Home page</div>;
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                Discover & Share
+                <br className="max-md:hidden" />
+                <section className="orange_gradient text-center">AI-Powered Prompts</section>
+            </h1>
+            <p className="desc text-center">
+                This is an AI prompting tool to discover, create and share creative prompts
+            </p>
+        </section>
+    );
 };
 
 export default Home;
